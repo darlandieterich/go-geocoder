2b972797dcb4507ffd388331af93ada6e0f174a3
darlandieterich <darlandieterich@gmail.com>
2021-01-24 17:56:19 -0300

    Implemented request struct

--- model/request.go
+++ model/request.go
@@ -1,1 +1,12 @@
 package model
+
+//GeocoderRequest - Struct for request
+type GeocoderRequest struct {
+	Street     string `json:"street"`
+	City       string `json:"city"`
+	County     string `json:"county"`
+	State      string `json:"state"`
+	Coutry     string `json:"coutry"`
+	PostalCode string `json:"postalCode"`
+	MaxResult  int    `json:"maxResult"`
+}
