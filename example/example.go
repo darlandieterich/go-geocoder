cb3d49ef5bdc00423dda361ffe6ae65e7f606ae0
darlandieterich <darlandieterich@gmail.com>
2021-02-27 17:13:24 -0300

    Updated example file

--- example/example.go
+++ example/example.go
@@ -9,16 +9,16 @@ import (
 
 func main() {
 	req := model.GeocoderRequest{
-			City: "Panambi",
+			City: "Ijui",
 			Country: "Brasil",
 	}
 	g := geocoder.New()
 	g.Timeout = 8 //seconds
-	ret := g.Search(req).ToObject().Find("display_name", "Ijui")
-	/* m := ret.Result
-	fmt.Println(m) */
-	/* for i := range m {
-		fmt.Println(fmt.Sprintf("Result: %d, %s", i, m[i]["display_name"]))
-	} */
-	fmt.Println(ret.Result)
+	ret := g.Search(req).ToObject() //.Find("display_name", "Ijui")
+
+	for i := range ret.Result {
+		fmt.Println(fmt.Sprintf("Result: %d, %s", i, ret.Result[i]["display_name"]))
+	}
+	//fmt.Println("Result", ret.Result)
+	fmt.Println("Error", ret.Error)
 }
