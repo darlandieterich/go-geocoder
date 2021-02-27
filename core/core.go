734725029434a1948e3f8e8d16714424358a7eb8
darlandieterich <darlandieterich@gmail.com>
2021-02-27 17:06:16 -0300

    Implemented the main functions of core geocoder

---
+++ core/core.go
@@ -0,0 +1,53 @@
+package core
+
+import (
+  "strings"
+	"encoding/json"
+
+	api   "../api"
+	model "../model"
+)
+
+type Object model.Object
+
+type Config model.GeocoderConfig
+
+//Search - Search to get the coordenates
+func (c *Config) Search(r model.GeocoderRequest) *Object {
+	castObject := (*model.GeocoderConfig)(c)
+	req := api.Object{}
+	req.Config = *castObject
+	returnCast := (*Object)(req.Request(r))
+	return returnCast
+}
+
+//ReverseSearch - Reverse Search
+func (c *Config) ReverseSearch() {
+
+}
+
+//Find - Search by element with name and contains value
+func (o *Object) Find(element string, value string) *Object {
+	if o.Error == nil {
+		var matches []map[string]interface{}
+		for _, item := range o.Result {
+			if item[element] != nil {
+				if strings.Contains(strings.ToLower(item[element].(string)), strings.ToLower(value)) {
+					matches = append(matches, item)
+				}
+			}
+		}
+		o.Result = matches
+	}
+	return o
+}
+
+//ToObject - Function to return object in time execution
+func (o *Object) ToObject() *Object {
+	if o.Error == nil {
+		var results []map[string]interface{}
+		json.Unmarshal([]byte(o.Internal.(string)), &results)
+		o.Result = results
+	}
+	return o
+}
