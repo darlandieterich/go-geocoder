acd04a3d1f87a7bc8e2daeb27f2829d9ce11b5fb
darlandieterich <darlandieterich@gmail.com>
2021-03-02 23:55:27 -0300

    Added url api reverse

--- model/api.go
+++ model/api.go
@@ -1,3 +1,5 @@
 package model
 
-const UrlAPI string = `https://nominatim.openstreetmap.org/search.php?polygon_geojson=1&format=jsonv2`
+const UrlAPISearch string = `https://nominatim.openstreetmap.org/search.php?polygon_geojson=1&format=jsonv2`
+
+const UrlAPIReverse string = `https://nominatim.openstreetmap.org/reverse.php?&format=jsonv2`
